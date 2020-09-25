8bfde45b45892fe8de83750c37188e5fde8ccd76
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2020-09-25 20:56:14 -0300

    Add credit card issuers

---
+++ datagenerator/issuers.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+'''
+List of issuers to be used when creating a CreditCard object.
+
+Issuers without a location are considered global (multinational).
+
+Please report if any name or location is wrong 
+so that this list can be corrected.
+'''
+companies = {
+              'American Express': None,
+              'Discover': 'US',
+              'Capital One': 'US',
+              'Wells Fargo': 'US',
+              'Bank of America': 'US',
+              'Chase': 'US',
+              'Citi': None,
+              'U.S. Bank': 'US',
+              'Barclays': None,
+              'Bradesco': 'BR',
+              'Itaucard': 'BR',
+              'Ourocard': 'BR',
+              'Caixa Econômica Federal': 'BR',
+              'Nubank': None,
+              'Santander': None,
+              'Inter': 'BR',
+              'BPCE': 'FR',
+              'BNP Paribas': 'FR',
+              'CartaSi': 'IT',
+              'HSBC': None,
+              'Yapi Kredi Bankasi': 'TR',
+              'RBS/NatWest': 'GB',
+              'Garanti Bank': 'TR',
+              'Societé Generale': 'FR',
+              'Lloyds Banking Group': 'GB',
+              'Agricultural Bank': 'CN',
+              'ICBC': 'CN',
+              'China Construction Bank': 'CN',
+              'Bank of China': 'CN',
+              'China Merchants Bank': 'CN',
+              'Bank of Communications': 'CN',
+              'China Everbright Bank': 'CN',
+              'Commonwealth Bank': 'AU',
+              'China Minsheng Bank': 'CN',
+              'Mitsubishi UFJ Nicos': 'JP',
+              'BMW Card': 'DE',
+              'TF Bank': 'DE',
+              'Advanzia Bank': 'DE',
+              'DKB': 'DE',
+              'N26': 'DE',
+              'Deutschland-Kreditkarte': 'DE',
+              'HDFC Bank': 'IN',
+              'SBI Card': 'IN',
+              'ICICI Bank': 'IN',
+              'Axis Bank': 'IN',
+              'YES Bank': 'IN',
+              'Standard Chartered Bank': 'IN',
+              'IndusInd Bank': 'IN',
+              'RBL Bank': 'IN',
+              'Bank of Baroda': 'IN',
+}
+
