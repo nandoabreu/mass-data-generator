4b09cf09b71f0a4bdebbd532e63b7fca7a5a8d73
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2020-09-25 20:55:53 -0300

    Add namefake.com API available languages

---
+++ datagenerator/countries.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+'''
+List of available countries to be used when creating a Person object.
+All languages available by namefake.com in 2020-09-24 are listed.
+
+This list may vary from namefake.com, once we are parsing data from
+namefake.com, but have no control over the namefake.com website.
+'''
+languages = {
+              'es_AR': 'spanish-argentina',
+              'hy_AM': 'armenian-armenia',
+              'en_AU': 'english-australia',
+              'de_AT': 'german-austria',
+              'bn_BD': 'bengali-bangladesh',
+              'nl_BE': 'dutch-belgium',
+              'fr_BE': 'french-belgium',
+              'pt_BR': 'portuguese-brazil',
+              'bg_BG': 'bulgarian-bulgaria',
+              'en_CA': 'english-canada',
+              'fr_CA': 'french-canada',
+              'cs_CZ': 'czech-czech-republic',
+              'da_DK': 'danish-denmark',
+              'fi_FI': 'finnish-finland',
+              'fr_FR': 'french-france',
+              'ka_GE': 'georgian-georgia',
+              'de_DE': 'german_germany',
+              'el_GR': 'greek-greece',
+              'hu_HU': 'hungarian-hungary',
+              'is_IS': 'icelandic-iceland',
+              'id_ID': 'indonesian-indonesia',
+              'fa_IR': 'persian_Iran',
+              'it_IT': 'italian-italy',
+              'ja_JP': 'japanese-japan',
+              'ar_JO': 'abic-jordan' ,
+              'kk_KZ': 'kazakh-kazakhstan',
+              'lv_LV': 'latvian-latvia',
+              'ro_MD': 'romanian-moldova',
+              'me_ME': 'montenegro-montenegrin',
+              'ne_NP': 'nepali-nepal',
+              'nl_NL': 'dutch-netherlands',
+              'en_NZ': 'english-new-zealand',
+              'nn_NO': 'norwegian-norway',
+              'es_PE': 'spanish-peru',
+              'en_PH': 'english-philippines',
+              'pl_PL': 'polish-poland',
+              'pt_PT': 'portuguese-portugal',
+              'ro_RO': 'romanian-romania',
+              'ru_RU': 'russian-russia',
+              'sk_SK': 'slovak-slovakia',
+              'sl_SI': 'slovenian-slovenia',
+              'en_ZA': 'english-south-africa',
+              'ko_KR': 'korean-south-korea',
+              'es_ES': 'spanish-spain',
+              'en_UG': 'english-uganda',
+              'en_GB': 'english-united-kingdom',
+              'en_US': 'english-united-states',
+              'es_VE': 'spanish-venezuela',
+}
+
